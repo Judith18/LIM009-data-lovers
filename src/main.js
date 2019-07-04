9c4cc106f2aa201c224bbc9c18fc01642abd30e9
JudithCristina <judicriss18@gmail.com>
2019-07-04 16:42:52 -0500

    Realizando graficos google chart

--- src/main.js
+++ src/main.js
@@ -6,8 +6,59 @@ const indicatorsPage = document.getElementById('indicators-page');
 const dataIndicatorsPage = document.getElementById('page-data-indicators');
 const btnIndicators = document.getElementById("indicators");
 const tablaDataIndicators = document.getElementById("tabla-data");
+const tablaTotal = document.getElementById('dateTotal');
+const grafico = document.getElementById("chart_div");
+const grafico1 = document.getElementById("chart_div1");
 const tablaEstadist = document.getElementById("tabla-estadist");
-const orderDataBtn = document.getElementById("order-data-btn");
+const orderDataBtn = document.getElementsByName("dataOrder");
+const dataOrder = document.getElementById('dato-order')
+const radTypeGraf = document.getElementById('style-graf')
+const datosEstadisticos = document.getElementsByName("datosEstats");
+const typeGraficos = document.getElementsByName('typeGrafico');
+
+
+
+for (let i = 0; i < datosEstadisticos.length; i++) {
+  datosEstadisticos[i].addEventListener("change", () => {
+    if (datosEstadisticos[i].value === 'Tabla General') {
+      tablaTotal.classList.remove('hide')
+      tablaTotal.classList.add('show')
+      grafico1.classList.remove('show')
+      grafico1.classList.add('hide')
+      tablaEstadist.classList.remove('show')
+      tablaEstadist.classList.add('hide')
+      dataOrder.classList.remove('hide')
+      dataOrder.classList.add('show')
+      radTypeGraf.classList.remove('show')
+      radTypeGraf.classList.add('hide')
+    }
+    else if (datosEstadisticos[i].value === 'Gráficos') {
+      grafico1.classList.remove('hide')
+      grafico1.classList.add('show')
+      tablaEstadist.classList.remove('show')
+      tablaEstadist.classList.add('hide')
+      tablaTotal.classList.remove('show')
+      tablaTotal.classList.add('hide')
+      dataOrder.classList.remove('hide')
+      dataOrder.classList.add('show')
+      radTypeGraf.classList.remove('hide')
+      radTypeGraf.classList.add('show')
+    } else {
+      grafico1.classList.remove('show')
+      grafico1.classList.add('hide')
+      tablaEstadist.classList.remove('hide')
+      tablaEstadist.classList.add('show')
+      tablaTotal.classList.remove('show')
+      tablaTotal.classList.add('hide')
+      dataOrder.classList.remove('show')
+      dataOrder.classList.add('hide')
+      radTypeGraf.classList.remove('show')
+      radTypeGraf.classList.add('hide')
+    }
+  })
+}
+
+
 
 let pages = (pageToShow) => {
   [homePage, indicatorsPage, dataIndicatorsPage].forEach(page => {
@@ -25,6 +76,7 @@ home.addEventListener('click', () => {
 indicatorsP.addEventListener('click', () => {
   pages(indicatorsPage);
 });
+
 
 btnIndicators.addEventListener('click', () => {
   const country = document.getElementById("country").value;
@@ -55,36 +107,129 @@ btnIndicators.addEventListener('click', () => {
   datosList.forEach(dato => {
     dato.addEventListener('click', () => {
       pages(dataIndicatorsPage);
-      let dataIndividual="";
+      let dataIndividual = "";
       const indicatorId = dato.id;
       // mostrar el nombre del indicador en la pagina 3
       const indicatorSelected = document.getElementById('paint-indicator');
       indicatorSelected.innerHTML = dato.textContent;
       returnIndicatorsData = window.worldBank.indicatorData(listFemIndicators, indicatorId);
-
+      let myValor = [];
       for (let i in returnIndicatorsData) {
         if (returnIndicatorsData[i] !== "") {
           dataIndividual +=
             ` <tr><td> ${i} </td>
                 <td>${ returnIndicatorsData[i].toFixed(2)}</td></tr>`;
           tablaDataIndicators.innerHTML = dataIndividual;
+          myValor.push([i, returnIndicatorsData[i]]);
         }
       }
-      orderDataBtn.addEventListener('click', () => {
-        tablaDataIndicators.innerHTML = "";
-        const selectOrder = document.getElementById('select-order').value;
-        let returnOrderDataTable = window.worldBank.orderDataTable(returnIndicatorsData, selectOrder);
-        let dataOrderIndividual = "";
-        for (let value of returnOrderDataTable) {
-          if (value[1] !== "") {
-            dataOrderIndividual +=
-              ` <tr><td> ${value[0]} </td>
-                   <td>${ value[1].toFixed(2)}</td></tr>`;
-            tablaDataIndicators.innerHTML = dataOrderIndividual;
-          }
+
+      google.charts.load('current', { 'packages': ['corechart'] });
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        const data = new google.visualization.DataTable();
+        data.addColumn('string', 'Años');
+        data.addColumn('number', 'Porcentajes');
+        data.addRows(myValor);
+
+        const options = {
+          'title': `${dato.textContent} `,
+          titleTextStyle: {
+            color: 'blue',
+            fontName: 'Arial',
+            fontSize: 16
+          },
+          chartArea: { left: '2px', top: '2px', right: '0.5px', width: "70%", height: "70%" },
+          hAxis: { title: 'Años', titleTextStyle: { color: 'blue' } },
+          vAxis: { title: 'Porcentajes', titleTextStyle: { color: 'blue' } },
+          'width': '100%',
+          'height': '410px',
+          'colors': ['blue'],
+          'backgroundColor': '#EFF2FB',
+          series: [{ color: 'blue', visibleInLegend: false }],
+          titlePosition: 'center',
+          max: { color: 'red' },
+          is3D: true
+        };
+        for (let i = 0; i < datosEstadisticos.length; i++) {
+          datosEstadisticos[i].addEventListener("change", () => {
+            if (datosEstadisticos[i].value === 'Gráficos') {
+              const chart = new google.visualization.LineChart(grafico);
+              chart.draw(data, options);
+            }
+          });
         }
-        // console.loh('order', dataIndividual)
-      });
+
+
+        for (let i = 0; i < typeGraficos.length; i++) {
+          typeGraficos[i].addEventListener("change", () => {// Instantiate and draw our chart, passing in some options.
+            const chart = new google.visualization[typeGraficos[i].value](grafico);
+            chart.draw(data, options);
+          }
+          )
+        }
+      }
+
+      for (let i = 0; i < orderDataBtn.length; i++) {
+        orderDataBtn[i].addEventListener("change", () => {
+          tablaDataIndicators.innerHTML = "";
+          grafico.innerHTML = "";
+          const selectOrder = orderDataBtn[i].value;
+          let returnOrderDataTable = window.worldBank.orderDataTable(returnIndicatorsData, selectOrder);
+          let dataOrderIndividual = "";
+          let myValorOrder = [];
+          for (let value of returnOrderDataTable) {
+            if (value[1] !== "") {
+              dataOrderIndividual +=
+                ` <tr><td> ${value[0]} </td>
+                   <td>${ value[1].toFixed(2)}</td></tr>`;
+              tablaDataIndicators.innerHTML = dataOrderIndividual;
+              myValorOrder.push([value[0], value[1]]);
+            }
+          }
+
+          google.charts.load('current', { 'packages': ['corechart'] });
+          google.charts.setOnLoadCallback(drawChart);
+
+          function drawChart() {
+            const data = new google.visualization.DataTable();
+            data.addColumn('string', 'Años');
+            data.addColumn('number', 'Porcentajes');
+            data.addRows(myValorOrder);
+
+            const options = {
+              'title': `${dato.textContent} `,
+              titleTextStyle: {
+                color: 'blue',
+                fontName: 'Arial',
+                fontSize: 16
+              },
+              chartArea: { left: '2px', top: '2px', right: '0.5px', width: "70%", height: "70%" },
+              hAxis: { title: 'Años', titleTextStyle: { color: 'blue' } },
+              vAxis: { title: 'Porcentajes', titleTextStyle: { color: 'blue' } },
+              'width': '100%',
+              'height': '400px',
+              'colors': ['blue'],
+              'backgroundColor': '#EFF2FB',
+              series: [{ color: 'blue', visibleInLegend: false }],
+              titlePosition: 'center',
+              max: { color: 'red' },
+              is3D: true
+            };
+
+    
+            for (let i = 0; i < typeGraficos.length; i++) {
+              typeGraficos[i].addEventListener("change", () => {
+                const chart = new google.visualization[typeGraficos[i].value](grafico);
+                chart.draw(data, options);
+              }
+              )
+            }
+          }
+          // console.loh('order', dataIndividual)
+        });
+      }
       let arrayData = Object.values(returnIndicatorsData);
       let arrayFilterNumberData = arrayData.filter(Number);
       let minData = Math.min(...arrayFilterNumberData).toFixed(2);
@@ -105,3 +250,7 @@ btnIndicators.addEventListener('click', () => {
     });
   });
 });
+
+///mi grafico//
+
+
